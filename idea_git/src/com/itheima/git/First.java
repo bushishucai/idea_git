6bebc045ef06deef36e74703cd9c71caa9aa4083
lichangzhen <1027284894@qq.com>
2018-12-21 09:38:58 +0800

    把本地工程的修改提交到git

--- idea_git/src/com/itheima/git/First.java
+++ idea_git/src/com/itheima/git/First.java
@@ -4,4 +4,8 @@ public class First {
     public static void main(String[] args) {
         System.out.println(111);
     }
+
+    public void tset(){
+        System.out.println("fjdoiaj;fklds");
+    }
 }
